abdb361960aec903b68ba1108ae8b6ecdc031688
VueVindicator <thatjsprof@gmail.com>
2021-07-21 14:02:58 +0100

    feat: add reselect library for memoization in redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import { connect } from "react-redux";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping.svg";
 import { toggleHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleHidden, itemCount }) => {
+const CartIcon = ({ toggleHidden, itemsCount }) => {
   return (
     <div className="cart-icon" onClick={toggleHidden}>
       <ShoppingIcon className="shopping-icon"></ShoppingIcon>
-      <span className="item-count">{itemCount}</span>
+      <span className="item-count">{itemsCount}</span>
     </div>
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.length || 0,
+const mapStateToProps = (state) => ({
+  itemsCount: selectCartItemsCount(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./cart-item.styles.scss";
+
+const CartItem = ({ item: { name, quantity, price, imageUrl } }) => {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt={name}></img>
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">${quantity * price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
