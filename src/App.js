6af74ea5c3812f0a55e1c078459c9672b9420e4e
VueVindicator <thatjsprof@gmail.com>
2021-07-21 05:04:55 +0100

    feat: setup map state and map dispatch functions for user reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch, Link } from "react-router-dom";
+import { connect } from "react-redux";
 import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
@@ -7,33 +8,26 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfile } from "./firebase/firebase.utils";
 
-class App extends React.Component {
-  constructor() {
-    super();
+import { setCurrentUser } from "./redux/user/user.actions";
 
-    this.state = {
-      currentUser: null,
-    };
-  }
+class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = createUserProfile(userAuth);
         (await userRef).onSnapshot((snapshot) => {
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data(),
-            },
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data(),
           });
         });
       }
-      this.setState({
-        currentUser: userAuth
-      })
+      setCurrentUser(userAuth)
     });
   }
 
@@ -55,4 +49,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { ReactComponent as Logo } from '../../assets/crown.svg'
-import './header.styles.scss'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
+import "./header.styles.scss";
 
-const Header = () => {
-    return (
-        <div className="header">
-            <Link className="logo-container" to="/">
-                <Logo className="logo"></Logo>
-            </Link>
-            <div className="options">
-                <Link className="option" to="/shop">
-                    Shop
-                </Link>
-                <Link className="option" to="/contact">
-                    Contact
-                </Link>
-            </div>
-        </div>
-    )
-}
+const Header = ({ currentUser }) => {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo"></Logo>
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/contact">
+          Contact
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={async () => await auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
