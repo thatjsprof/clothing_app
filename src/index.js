f322852f7de21ad3e39e483c7ca80a8dbda5eccd
VueVindicator <thatjsprof@gmail.com>
2021-07-22 22:42:00 +0100

    feat: add redux persist package to project

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import Store from './redux/store'
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <Provider store={Store}>
+  <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import UserReducer from "./user/user.reducer";
 import CartReducer from './cart/cart.reducer'
 
-export default combineReducers({
-  user: UserReducer,
-  cart: CartReducer
-});
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
+    user: UserReducer,
+    cart: CartReducer
+  });
+
+export default persistReducer(persistConfig, rootReducer)
